bebc937c054eea76ff8903fa37e4839ddb2910fd
michaelsrr <80532798+michaelsrr@users.noreply.github.com>
2021-05-30 23:24:13 -0500

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,18 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCQkEEdcwwoXtQqgkjpgdzkCCqjQ164KN4",
+    authDomain: "proyect-tec-1f411.firebaseapp.com",
+    databaseURL: "https://proyect-tec-1f411-default-rtdb.firebaseio.com",
+    projectId: "proyect-tec-1f411",
+    storageBucket: "gs://proyect-tec-1f411.appspot.com",
+    messagingSenderId: "222158333750",
+    appId: "1:222158333750:web:834ea07fcdc78a512cb597",
+    measurementId: "G-9RLGKMNBE5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+console.log(firebase);
+
+
+
